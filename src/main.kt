b038e2cbb61044ca6dc6d3cd9b8616e620903182
Munde-J <mundeangelah@gmail.com>
2022-03-10 09:00:59 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,28 @@
+import kotlin.math.abs
+
+fun main(){
+
+var remain=(7,3)
+    println
+    //var remainder
+}
+fun remain(a:Int,b:Int){
+    var mod= a%b
+
+}
+fun place(name:String){
+    var school="codeHive"
+    var stmt=school[4]
+    var stmt2=school[5]
+    var stmt3=school[6]
+    var stmt4=school[7]
+var stmt5=("stmt;stmt2;stmt3;stmt4")
+println()
+
+}
+fun name(a:String){
+    var a="watermelon".length
+    println()
+
+
+}
